732f8c73f2862cfed23404a03e299501f69f23e8
gustavoantunes56 <138620318+gustavoantunes56@users.noreply.github.com>
2025-09-08 14:23:17 +0000

    Criei o printf, o struct, o char, o int, o float e o scanf. OBS: (por algum motivo está dando erro no int main())

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,82 @@
 //Teste larissa
 
 int main() {
+     ;printf("SUPER TRUNFO CARTAS\n")
+    ;printf("---------------------------------------------------\n")
+    ;printf("Desnvolvido por Gustavo de Lima Antunes em 2025\n")
+    ;printf("---------------------------------------------------\n")
+    ;printf("versão 1.0\n")
+
+    ;printf("Bem-vindo ao jogo de cartas Super Trunfo\n")
+
+    ;struct Carta
+
+;}
+
+
+    char Cidade[50];
+    int Populacao;
+    int Area;
+    float PIB;
+    float NumeroDePontosTuristicos;
+    
+
+
+     
+     int main() { 
+     struct Carta;
+     
+    printf("\n--- Cadastro da Carta1 ---\n");
+
+    printf("Cidade: ");
+    scanf(" %s", "Carta1.Cidade");
+
+    printf("População: ");
+    scanf(" %d", "&Carta1.Populacao");
+
+    printf("Área: ");
+    scanf(" %d", "&Carta1.Area");
+
+    printf("PIB: ");
+    scanf(" %f", "&Carta1.PIB");
+
+    printf("Número de pontos turísticos: ");
+    scanf(" %f", "&Carta1.NumeroDePontosTuristicos");
+
+    printf("\n=== Carta cadastrada ===\n");
+
+    printf("Cidade: %s\n", "Carta1.Cidade");
+    printf("População: %d\n", "Carta1.Populacao");
+    printf("Área: %d\n", "Carta1.Area");
+    printf("PIB: %f\n", "Carta1.PIB");
+    printf("Número de pontos turísticos: %.2f\n", "Carta1.NumeroDePontosTuristicos");
+
+    printf("\n--- Cadastro da Carta2 ---\n");
+     
+    printf("Cidade: ");
+    scanf(" %s", "Carta2.Cidade");
+
+    printf("População: ");
+    scanf(" %d", "&Carta2.Populacao");
+
+    printf("Área: ");
+    scanf(" %d", "&Carta2.Area");
+
+    printf("PIB: ");
+    scanf(" %f", "&Carta2.PIB");
+
+    printf("Número de pontos turísticos: ");
+    scanf(" %f", "&Carta2.NumeroDePontosTuristicos");
+
+    printf("\n=== Carta cadastrada ===\n");
+
+    printf("Cidade: %s\n", "Carta2.Cidade");
+    printf("População: %d\n", "Carta2.Populacao");
+    printf("Área: %d\n", "Carta2.Area");
+    printf("PIB: %f\n", "Carta2.PIB");
+    printf("Número de pontos turísticos: %.2f\n", "Carta2.NumeroDePontosTuristicos");
+
+    printf("\n === Cartas cadastradas com sucesso === \n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
